f3d13c67c282e36395e0c9e064ea02f81448fae9
Ly Minh Phuong <ly.minh.phuong.1403@gmail.com>
2011-03-07 18:14:45 +0700

    first basic command

--- src/main/java/to/us/phuonglm/jackRabbitSample/App.java
+++ src/main/java/to/us/phuonglm/jackRabbitSample/App.java
@@ -1,7 +1,6 @@
 package to.us.phuonglm.jackRabbitSample;
 
 import java.io.IOException;
-
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
@@ -42,11 +41,11 @@ public class App
 		}*/
     	
     	
-    	BasicConfigurator.configure();
     	
     	//JCR Command Line
     	try {
 			JCRCommandLine commandLine = new JCRCommandLine();
+			commandLine.startCMD();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			logger.error(e);
--- src/main/java/to/us/phuonglm/jackRabbitSample/JCRCommandLine.java
+++ src/main/java/to/us/phuonglm/jackRabbitSample/JCRCommandLine.java
@@ -1,8 +1,20 @@
 package to.us.phuonglm.jackRabbitSample;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
+import javax.jcr.LoginException;
+import javax.jcr.Node;
+import javax.jcr.NodeIterator;
+import javax.jcr.Repository;
+import javax.jcr.RepositoryException;
+import javax.jcr.Session;
+import javax.jcr.SimpleCredentials;
+import javax.jcr.Workspace;
+
+import org.apache.jackrabbit.commons.NamespaceHelper;
+import org.apache.jackrabbit.commons.iterator.NodeIterable;
+import org.apache.jackrabbit.core.TransientRepository;
 import org.apache.log4j.Logger;
 import org.apache.log4j.LogManager;
 
@@ -13,32 +25,90 @@ import jline.NullCompletor;
 import jline.SimpleCompletor;
 
 public class JCRCommandLine {
-	private static final Logger logger = LogManager.getLogger(JCRCommandLine.class);
+	private static final Logger logger = LogManager
+			.getLogger(JCRCommandLine.class);
 
-	private String[] syntax = { "foo", "bar", "baz" };
-	private Completor[] completors = {
-			new SimpleCompletor(new String[] { "foo", "bar", "baz" }),
-			new SimpleCompletor(new String[] { "xxx", "yyy", "xxx" }),
-			new NullCompletor()
-	};
+	private String[] syntaxs = { "makeNode", "deleteNode", "changeNode", "listNode", "listProperty", "getProperty", "setProperty", "exit" };
+
+	private Node currentNode;
+
+	private Completor[] completors = { new SimpleCompletor(syntaxs),
+			new NullCompletor() };
 
 	private ConsoleReader consoleReader;
 
 	private void init() throws IOException {
-		consoleReader = new ConsoleReader();
+		consoleReader = new ConsoleReader(); 
 		consoleReader.addCompletor(new ArgumentCompletor(completors));
-
 	}
 
-	public JCRCommandLine() throws IOException {
-		String command = "";
-		String prependString = "";
-		init();
-
-		while (!command.equals("exit")) {
-			command = consoleReader.readLine();
-			System.out.println("Command: " + command);
-			System.out.print(prependString);
+	private void cmdProcess(Node currentNode, String command) throws RepositoryException{
+		if(command.startsWith("listNode")){
+			
+			NodeIterator nodeIterator = currentNode.getNodes();
+			while ( nodeIterator.hasNext()) {
+				System.out.println(nodeIterator.nextNode().getName() + " ");
+			}
+			
+		} else if(command.startsWith("makeNode ")) {
+			
+			currentNode.addNode(command.substring("makeNode ".length()));
+			
+		} else if (command.startsWith("changeNode ")) {
+			
+			String changeTo = command.substring("changenode ".length());
+			if(changeTo.equals("..")){
+				currentNode = currentNode.getParent();
+			} else {
+				currentNode = currentNode.getNode(command.substring("changenode ".length()));
+			}
+			
+		} else if ( command.startsWith("deleteNode ")) {
+			
+			String nodeToDelete = command.substring("deleteNode ".length());
+			if (!nodeToDelete.isEmpty()){
+				
+				currentNode.getNode(nodeToDelete).remove();	
+			}
 		}
+	}
+	
+	public JCRCommandLine() throws IOException {
+		init();
+	}
+	
+	public void startCMD() throws IOException {
+		String command = "";
+		Repository repository = new TransientRepository(new File("/tmp/jcr"));
+		Session session = null;
+		try {
+			session = repository.login(new SimpleCredentials("phuonglm", new char[0]));
+			if((new NamespaceHelper(session).getURI("exo") == null)){
+				session.getWorkspace().getNamespaceRegistry().registerNamespace("exo", "http://jackrabbit.apache.org/exo");
+			}
+			currentNode = session.getRootNode();
+			while (!command.equals("exit")) {
+				consoleReader.setDefaultPrompt(currentNode.getPath() + "/:");
+				command = consoleReader.readLine();
+				command = command.trim();
+				try {
+					cmdProcess(currentNode, command);
+					session.save();
+				} catch (RepositoryException e) {
+					System.out.println(e.toString());
+				}
+			}
+			
+		} catch (LoginException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (RepositoryException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if(session != null){
+				session.logout();
+			}
+        }
 	}
 }
